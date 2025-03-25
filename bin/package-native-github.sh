441d061a6ff983fcd65bb00dd49e669656fc242c
xiaochun <90xchun@gmail.com>
2025-03-25 22:47:08 +0800

    Add arm64 build

--- bin/package-native-github.sh
+++ bin/package-native-github.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-basePath=${1}
-mkdir -p ${basePath}
-echo "real target folder ${basePath}"
 
 java -version
 ./mvnw clean package
 ./mvnw -Pnative -Dagent exec:exec@java-agent -U
 ./mvnw -Pnative package
+basePath=/tmp/download/plugin
+mkdir -p ${basePath}
 binName=sitemap
 if [ -f "target/${binName}.exe" ];
 then
@@ -14,8 +13,11 @@ then
   mv "target/${binName}.exe" "${basePath}/${binName}-Windows-$(uname -m).exe"
   exit 0;
 fi
-if [ -f "target/${binName}" ];
+if [[ "$(uname -s)" == "Linux" ]];
 then
-  echo "unix"
+  echo "Linux"
+  mv target/${binName} ${basePath}/${binName}-$(uname -s)-$(dpkg --print-architecture).bin
+else
+  echo "MacOS"
   mv target/${binName} ${basePath}/${binName}-$(uname -s)-$(uname -m).bin
 fi
